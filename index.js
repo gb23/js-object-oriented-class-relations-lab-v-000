a401d7ff68e8d888dc329b612aada91e649a82f9
gb23 <gregbenj@hotmail.com>
2017-10-22 17:49:24 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,84 @@
+// Driver class:
+// A driver has many trips, and has many passengers through trips.
+
+// trips() - returns all of the trips that a driver has taken
+// passengers() - returns all of the passengers that a driver has taken on a trip
+let store = {drivers: [], passengers: [], trips: []};
+let id = 0;
+class Driver{  
+    constructor(name){
+        this.name = name;
+        this.id = ++id;
+        store.drivers.push(this);
+    }
+    trips(){
+        return store.trips.filter(trip => {
+            return trip.driverId === this.id
+        });
+    }
+    passengers(){
+        return store.passengers.filter(passenger => {
+            for (const trip of passenger.trips()){
+                return trip.driverId === this.id;
+            } 
+        })
+    }
+}
+
+// Passenger class:
+// A passenger has many trips, and has many drivers through trips.
+// new Passenger() - initialized with a name; returns a JavaScript object that has attributes of id, and name
+// trips() - returns all of the trips that a passenger has taken
+// drivers() - returns all of the drivers that has taken a passenger on a trip
+class Passenger{
+    constructor(name){
+        this.name = name;
+        this.id = ++id;
+        store.passengers.push(this);
+    }
+    trips(){
+        return store.trips.filter(trip => {
+            return trip.passengerId === this.id;
+        });
+    }
+    // drivers(){
+    //     return this.trips().map((trip)=> {
+    //       return trip.driver();
+    //     });
+    // }
+    drivers(){
+        return store.drivers.filter(driver => {
+            for (const trip of driver.trips()){
+                return trip.passengerId === this.id;
+            }
+        });
+    }
+}
+
+// Trip class:
+// A trip belongs to a driver and belongs to a passenger.
+// new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that has attributes id, driverId, and passengerId
+// driver() - returns the driver associated with the trip
+// passenger() - returns the passenger associated with the trip
+class Trip{
+    constructor(driver, passenger){
+        this.id = ++id;
+        if (driver){
+            this.driverId = driver.id;
+        }
+        if (passenger){
+            this.passengerId = passenger.id;
+        }
+        store.trips.push(this);
+    }
+    passenger(){
+        return store.passengers.find(passenger =>{
+            return passenger.id === this.passengerId;
+        });
+    }
+    driver(){
+        return store.drivers.find(driver => {
+            return driver.id === this.driverId;
+        })
+    }
+}
